71d3e39873340f6721e2adaf4267e64cd7a9a8b5
Uriel <u@berlinblue.org>
2012-09-14 08:18:11 +0200

    Delete more obsolete comments.

--- image.go
+++ image.go
@@ -15,15 +15,8 @@ type vimage struct {
 	err error
 }
 
-// newImage is meant to be run as a goroutine and loads a decoded image into
-// an xgraphics.Image value and draws it to an X pixmap.
-// The loading doesn't start until this image's corresponding imgLoadChan
-// has been pinged.
-// This implies that all images are decoded on start-up and are converted
-// and drawn to an X pixmap on-demand. I am still deliberating on whether this
-// is a smart decision.
-// Note that this process, particularly image conversion, can be quite
-// costly for large images.
+// newImage loads a decodes an image into an xgraphics.Image value and draws it
+// to an X pixmap.
 func newImage(win *window, img *Img) *vimage {
 
 	start := time.Now()
